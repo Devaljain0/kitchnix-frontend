79cec540df053273e842e7b288a723f58d6e20fc
Devaljain0 <devaljain525@gmail.com>
2025-06-20 22:51:19 +0530

    all the routed are backed up

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,75 @@
 
- import { Route, Routes } from 'react-router-dom'
- import { Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom'
+import Login from './routes/Login'
+import Signup from './routes/Signup'
+import Otp from './routes/Otp'
+import { ProtectedRoute } from "./components/ProtectedRoute"; // Import the ProtectedRoute component
+import  Dashboard  from "./routes/Dashboard";
+import AdminDashboard from './routes/AdminDashboard';
+import { ProtectedAdminRoute } from './components/ProtectedAdminRoute';
+import AdminLogin from './routes/AdminLogin';
+import Inventory from './routes/inventory';
+import { ProtectedSupplierRoute } from './components/ProtectedSupplierRoute';
+import SupplierLogin from './routes/SupplierLogin';
+import UserCart from './routes/UserCart';
+import AllRecipes from './routes/AllRecipes';
+import OrderSummary from './routes/Order';
+import UserProfile from './routes/UserProfile';
 
- import Login from './routes/Login'
-// import Signup from './routes/Signup'
-// import Otp from './routes/Otp'
-// import { ProtectedRoute } from "./components/ProtectedRoute"; // Import the ProtectedRoute component
-// import  Dashboard  from "./routes/Dashboard";
-// import AdminDashboard from './routes/AdminDashboard';
-// import { ProtectedAdminRoute } from './components/ProtectedAdminRoute';
-// import AdminLogin from './routes/AdminLogin';
-// import Inventory from './routes/inventory';
-// import { ProtectedSupplierRoute } from './components/ProtectedSupplierRoute';
-// import SupplierLogin from './routes/SupplierLogin';
-// import UserCart from './routes/UserCart';
-// import AllRecipes from './routes/AllRecipes';
-// import OrderSummary from './routes/Order';
-// import UserProfile from './routes/UserProfile';
-console.log("App loaded")
- function App() {
-  
+function App() {
 
   return (
- 
-     <Routes>
-    
+  <div>
+    <Routes>
+    <Route path="/order" element={<OrderSummary/>} />
       <Route path="/login" element={<Login/>} />
-      <Route path="/" element={<Navigate to="/login" />} />
-      </Routes>
-     // <Route path="/order" element={<OrderSummary/>} />
-  //     <Route path="/signup" element={<Signup/>} />
-  //     <Route path="/verify" element={<Otp/>} />
-  //     <Route path="/adminLogin" element={<AdminLogin/>} />
-  //   <Route path='/supplierlogin' element={<SupplierLogin/>}/>
-  //   <Route path='/UserProfile' element={<UserProfile/>}/>
+        <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/verify" element={<Otp/>} />
+      <Route path="/adminLogin" element={<AdminLogin/>} />
+    <Route path='/supplierlogin' element={<SupplierLogin/>}/>
+    <Route path='/UserProfile' element={<UserProfile/>}/>
       
-  //     <Route path="/inventory" 
-  //     element={
-  //         <ProtectedSupplierRoute>
-  //         <Inventory/>
-  //         </ProtectedSupplierRoute>
-  //         } 
-  //       />
+      <Route path="/inventory" 
+      element={
+          <ProtectedSupplierRoute>
+          <Inventory/>
+          </ProtectedSupplierRoute>
+          } 
+        />
       
-  //     <Route
-  //         path="/dashboard"
-  //         element={
-  //           <ProtectedRoute>
-  //             <Dashboard />
-  //           </ProtectedRoute>
-  //         }
-  //       />
-  //        <Route
-  //         path="/AdminDashboard"
-  //         element={
-  //           <ProtectedAdminRoute>
-  //             <AdminDashboard/>
-  //           </ProtectedAdminRoute>
-  //         }
-  //       />
-  //       {/* All Recipes Route */}
-  //       <Route path="/allrecipes" element={<AllRecipes />} />
-  //       {/* User Cart Route */}
-  //       <Route
-  //         path="/cart"
-  //         element={
-  //           <ProtectedRoute>
-  //             <UserCart />
-  //           </ProtectedRoute>
-  //         }
-  //       />
+      <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+         <Route
+          path="/AdminDashboard"
+          element={
+            <ProtectedAdminRoute>
+              <AdminDashboard/>
+            </ProtectedAdminRoute>
+          }
+        />
+        {/* All Recipes Route */}
+        <Route path="/allrecipes" element={<AllRecipes />} />
+        {/* User Cart Route */}
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoute>
+              <UserCart />
+            </ProtectedRoute>
+          }
+        />
 
-  //   </Routes>
+    </Routes>
   
-  // </div>
-   )
+  </div>
+  )
 }
 
 export default App;
